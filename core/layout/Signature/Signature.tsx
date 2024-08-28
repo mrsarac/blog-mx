85eb56ed09a248740007a44a3c02115dab12cf8e
Mustafa <mrsarac@gmail.com>
2024-08-28 11:54:48 +0200

    signature

--- core/layout/Signature/Signature.tsx
+++ core/layout/Signature/Signature.tsx
@@ -1,4 +1,4 @@
-import { styled, Anchor, Text, Box, Flex } from '@maximeheckel/design-system';
+import { styled, Box, Flex } from '@maximeheckel/design-system';
 
 
 // const NewsletterForm = dynamic(() => import('@core/components/NewsletterForm'));
@@ -31,36 +31,7 @@ const Signature = ({ title, url }: { title: string; url: string }) => {
           margin: '0 auto',
         }}
         gap="5"
-      >
-        <Text as="p">
-          Liked this article? {/* @ts-ignore */}
-          <Anchor
-            favicon
-            href={`https://twitter.com/intent/tweet?text=${encodeURI(text)}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Share it with a friend on Twitter
-          </Anchor>{' '}
-          or{' '}
-          <Anchor
-            href="https://www.buymeacoffee.com/maximeheckel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            support me
-          </Anchor>{' '}
-          to take on more ambitious projects to write about. Have a question,
-          feedback or simply wish to contact me privately?{' '}
-          <Anchor
-            href="http://twitter.com/MaximeHeckel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Shoot me a DM
-          </Anchor>{' '}
-          and I&apos;ll do my best to get back to you.
-        </Text>
+      > 
 
         <Box>
           {/* <Text as="p" variant="primary">
