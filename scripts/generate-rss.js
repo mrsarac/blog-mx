4034833005d811499045fd176092352c8d568fe5
Mustafa <mrsarac@gmail.com>
2024-12-12 20:45:27 +0100

    updated rss

--- scripts/generate-rss.js
+++ scripts/generate-rss.js
@@ -16,19 +16,23 @@ const matter = require('gray-matter');
       .filter((name) => name !== 'img');
 
     const posts = files.reduce((allPosts, postSlug) => {
-      const source = fs.readFileSync(
-        path.join(root, 'content', postSlug),
-        'utf8'
-      );
-      const { data } = matter(source);
+      const fullPath = path.join(root, 'content', postSlug);
+      const stats = fs.statSync(fullPath);
 
-      return [
-        {
-          ...data,
-          slug: postSlug.replace('.mdx', ''),
-        },
-        ...allPosts,
-      ];
+      if (stats.isFile()) {
+        const source = fs.readFileSync(fullPath, 'utf8');
+        const { data } = matter(source);
+
+        return [
+          {
+            ...data,
+            slug: postSlug.replace('.mdx', ''),
+          },
+          ...allPosts,
+        ];
+      } else {
+        return allPosts;
+      }
     }, []);
 
     return posts;
@@ -36,12 +40,12 @@ const matter = require('gray-matter');
 
   try {
     const feed = new RSS({
-      title: "Maxime Heckel's Blog",
+      title: "Mustafa Saraç's Blog",
       description:
-        "Hi I'm Maxime, and this is my blog. Here, I share through my writing my experience as a frontend engineer and everything I'm learning about on React, Typescript, SwiftUI, Serverless, and testing.",
-      site_url: 'https://blog.maximeheckel.com',
-      feed_url: 'https://blog.maximeheckel.com/rss.xml',
-      image_url: 'https://blog.maximeheckel.com/static/og/main-og-image.png',
+        "Hi I'm Mustafa, and this is my blog.",
+      site_url: 'https://www.mustafasarac.com',
+      feed_url: 'https://www.mustafasarac.com/rss.xml',
+      image_url: 'https://www.mustafasarac.com/static/og/main-og-image.png',
       language: 'en',
     });
 
@@ -50,13 +54,13 @@ const matter = require('gray-matter');
     );
 
     content.forEach((post) => {
-      const url = `https://blog.maximeheckel.com/posts/${post.slug}`;
+      const url = `https://blog.mustafasarac.com/posts/${post.slug}`;
 
       feed.item({
         title: post.title,
         description: post.subtitle,
         date: new Date(post.date),
-        author: 'Maxime heckel',
+        author: 'Mustafa Saraç',
         url,
         guid: url,
       });
